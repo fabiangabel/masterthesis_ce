b040882831832d78e64a2d4a8d4365a76a8db6a5
Fabian Gabel <fabiangabel@hotmail.de>
2015-01-03 16:01:14 +0100

    CREATE FOLDER STRUCTURE

---
+++ cleanup.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+rm *.  masterthesis.aux masterthesis.bbl masterthesis.blg masterthesis.dvi masterthesis.ilg masterthesis.log masterthesis.nlo masterthesis.nls masterthesis.nomenclature.aux masterthesis.out masterthesis.pdf
