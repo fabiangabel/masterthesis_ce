7075c4d28a88a8e5104fe4c492c82559ef430e09
Fabian Gabel <fabiangabel@hotmail.de>
2015-01-04 00:27:23 +0100

    NEED TO START USING DIFFERENTIAL FORM OF EQUATIONS

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-rm *.  masterthesis.aux masterthesis.bbl masterthesis.blg masterthesis.dvi masterthesis.ilg masterthesis.log masterthesis.nlo masterthesis.nls masterthesis.nomenclature.aux masterthesis.out masterthesis.pdf
+rm masterthesis.aux masterthesis.bbl masterthesis.blg masterthesis.dvi masterthesis.ilg masterthesis.log masterthesis.nlo masterthesis.nls masterthesis.nomenclature.aux masterthesis.out masterthesis.pdf
+rm */*.aux
