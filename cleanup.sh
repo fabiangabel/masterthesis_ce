f5860c47acdd28b2a53ee915720c1a0ddc77af4f
Fabian Gabel <fabiangabel@hotmail.de>
2015-01-07 14:06:32 +0100

    ADD NEW TOC WITH DIFFERENT CHAPTERS FOR CPLD AND SEG

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
 
-rm masterthesis.aux masterthesis.bbl masterthesis.blg masterthesis.dvi masterthesis.ilg masterthesis.log masterthesis.nlo masterthesis.nls masterthesis.nomenclature.aux masterthesis.out masterthesis.pdf
-rm */*.aux
+rm -f masterthesis.aux
+rm -f masterthesis.bbl
+rm -f masterthesis.blg
+rm -f masterthesis.dvi
+rm -f masterthesis.ilg
+rm -f masterthesis.log
+rm -f masterthesis.nlo
+rm -f masterthesis.nls
+rm -f masterthesis.nomenclature.aux
+rm -f masterthesis.out
+rm -f masterthesis.toc
+rm -f */*.aux
