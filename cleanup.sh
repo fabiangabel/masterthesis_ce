a12289f43cb4c45f6f5fe84ea6dc77b17b91a918
Fabian Gabel <fabiangabel@hotmail.de>
2015-01-20 21:42:46 +0100

    ALMOST DONE WITH SIMPLE SECTION

--- cleanup.sh
+++ cleanup.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
 
+rm -f */*.aux
+rm -f masterthesis-blx.bib
 rm -f masterthesis.aux
 rm -f masterthesis.bbl
+rm -f masterthesis.bcf
 rm -f masterthesis.blg
 rm -f masterthesis.dvi
 rm -f masterthesis.ilg
+rm -f masterthesis.loa
+rm -f masterthesis.lof
 rm -f masterthesis.log
+rm -f masterthesis.lot
 rm -f masterthesis.nlo
 rm -f masterthesis.nls
 rm -f masterthesis.nomenclature.aux
 rm -f masterthesis.out
+rm -f masterthesis.pdf
+rm -f masterthesis.run.xml
 rm -f masterthesis.toc
-rm -f */*.aux
+rm -f texput.log
