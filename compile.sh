a150e325ad2df021982bbc42251e389212966903
Fabian Gabel <fabiangabel@hotmail.de>
2015-01-23 11:28:12 +0100

    PROOFREAD SEG SECTION, STILL MISSING SOME FIGURES

--- compile.sh
+++ compile.sh
@@ -2,6 +2,7 @@
 latex masterthesis.tex
 bibtex masterthesis.aux
 sed -i 's/, and/ and/g' masterthesis.bbl
+sed -i 's/Lilek, u\./Lilek, \\u{Z}\./g' masterthesis.bbl
 makeindex masterthesis.nlo -s nomencl.ist -o masterthesis.nls
 latex masterthesis.tex
 pdflatex masterthesis.tex
