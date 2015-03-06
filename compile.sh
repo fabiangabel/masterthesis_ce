0c6f38fe5c5fac6b59de7e0dd842035d3dddaacc
Fabian Gabel <gabel@mathematik.tu-darmstadt.de>
2015-03-07 00:49:57 +0100

    ADD SPEEDUP PLOTS

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-latex masterthesis.tex
+pdflatex masterthesis.tex
 bibtex masterthesis.aux
 sed -i 's/, and/ and/g' masterthesis.bbl
 sed -i 's/Lilek, u\./Lilek, \\u{Z}\./g' masterthesis.bbl
 makeindex masterthesis.nlo -s nomencl.ist -o masterthesis.nls
-latex masterthesis.tex
+pdflatex masterthesis.tex
 pdflatex masterthesis.tex
