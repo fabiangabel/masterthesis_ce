9bca1ec529c171a25ea6eac81e8850fd166f4b44
Fabian Gabel <gabel@mathematik.tu-darmstadt.de>
2015-03-09 16:34:42 +0100

    WRITE CONCLUSION AND CORRECT TYPOS

--- compile.sh
+++ compile.sh
@@ -3,6 +3,6 @@ pdflatex masterthesis.tex
 bibtex masterthesis.aux
 sed -i 's/, and/ and/g' masterthesis.bbl
 sed -i 's/Lilek, u\./Lilek, \\u{Z}\./g' masterthesis.bbl
-makeindex masterthesis.nlo -s nomencl.ist -o masterthesis.nls
+#makeindex masterthesis.nlo -s nomencl.ist -o masterthesis.nls
 pdflatex masterthesis.tex
 pdflatex masterthesis.tex
